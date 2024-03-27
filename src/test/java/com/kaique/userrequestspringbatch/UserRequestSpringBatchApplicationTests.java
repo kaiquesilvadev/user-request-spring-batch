2bd60bc90e2d486d15c5a2e69454c3b3ea103fc2
kaiquesilvadev <j.kaique1570@gmail.com>
2024-03-27 20:00:19 -0300

    first commit

---
+++ src/test/java/com/kaique/userrequestspringbatch/UserRequestSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaique.userrequestspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRequestSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
